3af9db2a1454a2d85000062b6c156536ef7bee01
Joseph Mania <60258622+maniamartial@users.noreply.github.com>
2022-12-25 17:38:03 +0300

    Create pickingnumbers.py

---
+++ pickingnumbers.py
@@ -0,0 +1,63 @@
+'''
+Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to .
+
+Example
+
+
+There are two subarrays meeting the criterion:  and . The maximum length subarray has  elements.
+
+Function Description
+
+Complete the pickingNumbers function in the editor below.
+
+pickingNumbers has the following parameter(s):
+
+int a[n]: an array of integers
+Returns
+
+int: the length of the longest subarray that meets the criterion
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    array1=[]
+    array2=[]
+    array3=[]
+    
+    a.sort(reverse=True)
+    L = []
+    for i in range(len(a)):
+        count = 0
+        for j in range(i+1,len(a)):
+            if abs(a[i] - a[j]) <=1:
+                count += 1
+        L.append(count)
+    return (max(L)+1)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
