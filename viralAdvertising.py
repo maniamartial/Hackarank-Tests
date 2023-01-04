a0165cfde03a7550712fb0caa6d5fe8eaeba0a0b
Joseph Mania <60258622+maniamartial@users.noreply.github.com>
2023-01-04 09:06:03 +0300

    Create viralAdvertising.py

---
+++ viralAdvertising.py
@@ -0,0 +1,68 @@
+'''
+
+HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly  people on social media.
+
+On the first day, half of those  people (i.e., ) like the advertisement and each shares it with  of their friends. At the beginning of the second day,  people receive the advertisement.
+
+Each day,  of the recipients like the advertisement and will share it with  friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day .
+
+Example
+.
+
+Day Shared Liked Cumulative
+1      5     2       2
+2      6     3       5
+3      9     4       9
+4     12     6      15
+5     18     9      24
+The progression is shown above. The cumulative number of likes on the  day is .
+
+Function Description
+
+Complete the viralAdvertising function in the editor below.
+
+viralAdvertising has the following parameter(s):
+
+int n: the day number to report
+Returns
+
+int: the cumulative likes at that day
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    likes=2
+    people=2
+    for i in range(2, n+1):
+        people=people*3
+        new_likes=people//2
+        likes+=new_likes
+        people=new_likes
+    return likes      
+    
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
